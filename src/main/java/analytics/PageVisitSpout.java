3b4c7299ce7d35cd2956859da9f177b4378067ec
salimmoulouel <salimmouloueletude@outlook.fr>
2020-12-13 17:00:12 +0100

    construction de deux bolt un pour les utilisateur et un autre pour les url

--- src/main/java/analytics/PageVisitSpout.java
+++ src/main/java/analytics/PageVisitSpout.java
@@ -11,33 +11,41 @@ import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Values;
 import org.apache.storm.utils.Utils;
 
-
 public class PageVisitSpout extends BaseRichSpout {
-	private SpoutOutputCollector outputCollector;
+	private static SpoutOutputCollector outputCollector;
+
+	@Override
+	public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+		outputCollector = collector;
+	}
+
 	@Override
 	public void nextTuple() {
-		// TODO Auto-generated method stub
 		String[] urls = {"http://example.com/index.html", "http://example.com/404.html", "http://example.com/subscribe.html"};
 		Integer[] userIds = {1, 2, 3, 4, 5};
 		
 		String url = urls[ThreadLocalRandom.current().nextInt(urls.length)];
 		Integer userId = userIds[ThreadLocalRandom.current().nextInt(userIds.length)];
-				
-		outputCollector.emit(new Values(url,userId));
+		
+		Values values = new Values(url, userId);
+		outputCollector.emit(values, values);
 		Utils.sleep(2000);
 	}
 
 	@Override
-	public void open(Map arg0, TopologyContext arg1, SpoutOutputCollector collector) {
-		// TODO Auto-generated method stub
-		outputCollector= collector;
-	}
-
-	@Override
 	public void declareOutputFields(OutputFieldsDeclarer declarer) {
-		// TODO Auto-generated method stub
-		declarer.declare(new Fields("url","userId"));
-
+		declarer.declare(new Fields("url", "userId"));
 	}
-
+	
+	@Override
+	public void ack(Object msgId) {
+		System.out.printf("Correctly processed: %s\n", msgId);
+	}
+	
+	@Override
+	public void fail(Object msgId) {
+		System.out.printf("ERROR processing: %s\n", msgId);
+		Values tuple = (Values)msgId;
+		outputCollector.emit(tuple, msgId);
+	}
 }
